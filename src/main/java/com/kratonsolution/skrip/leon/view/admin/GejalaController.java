0ea92ea339798c6e409ffc9a90e1ac2e6dde1d90
agung <agung@36.86.63.182>
2020-01-08 07:24:36 +0700

    update gejala module

--- src/main/java/com/kratonsolution/skrip/leon/view/admin/GejalaController.java
+++ src/main/java/com/kratonsolution/skrip/leon/view/admin/GejalaController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kratonsolution.skrip.leon.model.Gejala;
 
@@ -28,5 +30,21 @@ public class GejalaController {
 		model.addAttribute("gejalas", db.findAll(Gejala.class));
 		
 		return "gejala-home";
+	}
+	
+	@GetMapping("/admin/gejala-preadd")
+	public String preadd() {
+		return "gejala-add";
+	}
+	
+	@PostMapping("/admin/gejala-add")
+	public String add(@RequestParam String note) {
+		
+		Gejala gejala = new Gejala();
+		gejala.setNote(note);
+		
+		db.insert(gejala);
+		
+		return "redirect:/admin/gejala-home";
 	}
 }
